300d5a7a6a5402b1dae2f61bef427808c68a31ab
khalidnass <khalid.nass@gmail.com>
2022-03-05 11:33:25 +0300

    updated the setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'scikit-learn',
+      'scipy' 
+]
+
+test_requirements = ['pytest','Sphinx','rinohtype']
+  
 setup(
     name="mankey_stats",
     version="0.0.1",
@@ -13,9 +23,17 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dBlueG/mankey_stats",
     packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require = test_requirements, 
+    keywords=['python', 'first package'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education", 
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires = ">=3.6"
 )
