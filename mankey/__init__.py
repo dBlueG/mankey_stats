657ace43828126daf8cebf2a7fa155cf8abcb82d
ali-alqawaeen <ali.alqawaeen@gmail.com>
2022-03-16 14:16:08 +0300

    Update __init__.py

--- mankey/__init__.py
+++ mankey/__init__.py
@@ -1,15 +1,2 @@
 from . import mankey_dataframe, charting_helper, custom_helpers, stats_helpers
 __all__ = ["mankey_dataframe", "charting_helper", "custom_helpers", "stats_helpers"]
-
-
-import pathlib
-
-import mankey
-
-PACKAGE_ROOT = pathlib.Path(mankey.__file__).resolve().parent
-VERSION_PATH = PACKAGE_ROOT / "VERSION"
-
-name = "mankey"
-
-with open(VERSION_PATH, "r") as version_file:
-    __version__ = version_file.read().strip()
